f4e53f70f8c43fe89c9c866521ac86069de65909
Mara Vălean <125609119+MaraValean@users.noreply.github.com>
2023-04-12 15:39:36 +0300

    Add files via upload

---
+++ src/exceptions/InterpreterException.java
@@ -0,0 +1,12 @@
+
+package exceptions;
+
+public class InterpreterException extends Exception {
+    public InterpreterException() {
+        super();
+    }
+
+    public InterpreterException(String message) {
+        super(message);
+    }
+}
